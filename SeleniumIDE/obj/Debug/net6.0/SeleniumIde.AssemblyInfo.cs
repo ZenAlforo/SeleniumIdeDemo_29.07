b7010cfeef015eab56b8453921bc466768444548
Samuel Pankov <zenleviantalforo@gmail.com>
2024-08-01 21:33:24 +0300

    Changed YML file

--- SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
+++ SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af7e8c50ff63789bdea3fbd37401f05b123337bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
